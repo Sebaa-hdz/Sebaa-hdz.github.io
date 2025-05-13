4df35308fe31a68872be4f3895181292e106c363
Sebaa-hdz <1232200770@univermilenium.edu.mx>
2025-05-12 23:30:02 -0600

    Delete morales-boutique directory

--- morales-boutique/pla.js
+++
@@ -1,7 +0,0 @@
-function addToCart(item) {
-    alert(item + ' ha sido agregado al carrito.');
-}
-
-function readMore(url) {
-    window.location.href = url; // Redirige a la página del producto
-}
